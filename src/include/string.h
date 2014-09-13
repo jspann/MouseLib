d7c756883a3968391a6450f7db753f42041afc6e
James Spann <thedeveloper4@gmail.com>
2014-09-13 17:04:56 -0400

    Added string data type

---
+++ src/include/string.h
@@ -0,0 +1,18 @@
+//
+//  string.h
+//  
+//
+//  Created by James Spann on 9/13/14.
+//
+//
+
+#ifndef _string_h
+#define _string_h
+
+typedef struct String{
+    
+};
+
+int isequal(String x, String y);
+int getLen(String x);
+#endif
---
+++ src/string.c
@@ -0,0 +1,18 @@
+//
+//  string.c
+//  
+//
+//  Created by James Spann on 9/13/14.
+//
+//
+
+#include <string.h>
+
+int isequal(String x, String y){
+    for (int i=0; i<getLen(x); i++) {
+        
+    }
+}
+
+int getLen(String x){
+}
