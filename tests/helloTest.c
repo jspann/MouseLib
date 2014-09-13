c8bee19aa477d2a9b5b9b0e5e9344b5fe587c6fd
James Spann <thedeveloper4@gmail.com>
2014-09-13 19:57:16 -0400

    Added descriptions to the methods

--- tests/helloTest.c
+++ tests/helloTest.c
@@ -7,10 +7,13 @@
 //  A basic test file for the Mouselib library
 
 #include <math.h>
+#include <ctype.h>
+#include <string.h>
+#include <io.h>
+
 int main(){
-    int x = sin(0.5);
-    int y = pow(2,3);
-    int z = abs(12);
-    int xx = abc(-3.19)
-    return 0;
+    int x = sin(0.5);//Finds sine of .5
+    int y = pow(2,3);//Finds 2 to the power of 3
+    int z = abs(-12);//Finds the absolute value of negative 12
+    exit(0);//ends the program
 }
