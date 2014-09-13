e9b71546c643be3762e627f4909c9050e9243658
James Spann <thedeveloper4@gmail.com>
2014-09-13 15:53:05 -0400

    Added files for assert functions

---
+++ src/assert.c
@@ -0,0 +1,9 @@
+//
+//  assert.c
+//  
+//
+//  Created by James Spann on 9/13/14.
+//
+//
+
+#include <assert.h>
---
+++ src/include/assert.h
@@ -0,0 +1,13 @@
+//
+//  assert.h
+//  
+//
+//  Created by James Spann on 9/13/14.
+//
+//
+
+#ifndef _assert_h
+#define _assert_h
+
+
+#endif
