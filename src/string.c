8a4fe5a8a515f3d957cbc729c71d966f82b6d74d
James Spann <thedeveloper4@gmail.com>
2014-10-03 12:14:14 -0400

    Added new string functions

--- src/string.c
+++ src/string.c
@@ -15,4 +15,19 @@ int isequal(String x, String y){
 }
 
 int getLen(String x){
+    
+}
+
+void setValue(String x,const char *z){
+    for (int t = 0; t< strlen(z); t++){
+        
+    }
+}
+
+int strlen(const char *str){
+    const char *s;
+    
+    for (s = str; *s; ++s)
+        ;
+    return (s - str);
 }
